af7e4434a37fe70110db528f32168eb5abb71619
ccarrollATjhuecon <ccarroll@jhuecon>
2020-01-28 15:16:33 +0100

    Autoupdate start files

--- Virtual/Machine/VirtualBox/ISO-maker/finish-MAX.sh
+++ Virtual/Machine/VirtualBox/ISO-maker/finish-MAX.sh
@@ -62,10 +62,16 @@ sudo apt -y install curl git bash-completion xsel cifs-utils openssh-server naut
 # Extra packages for MAX
 sudo apt -y evince texlive-full quantecon scipy
 # Create a public key for security purposes
-sudo -u $myuser ssh-keygen -t rsa -b 4096 -q -N "" -C $myuser@XUBUNTU -f /home/econ-ark/.ssh/id_rsa
+sudo -u $myuser ssh-keygen -t rsa -b 4096 -q -N "" -C $myuser@XUBUNTU -f /home/@myuser/.ssh
 # Set up security for emacs package downloading 
 sudo apt -y install emacs
+sudo -u econ-ark emacs -batch -l ~/.emacs --eval='(package-list-packages)'
+sudo -u econ-ark mkdir -p /home/econ-ark/.emacs.d/elpa
+sudo -u econ-ark mkdir -p /home/econ-ark/.emacs.d/elpa/gnupg
 sudo -u econ-ark gpg --list-keys 
+sudo -u econ-ark gpg --homedir /home/econ-ark/.emacs.d/elpa       --list-keys
+sudo -u econ-ark gpg --homedir /home/econ-ark/.emacs.d/elpa/gnupg --list-keys
+sudo -u econ-ark gpg --homedir /home/econ-ark/.emacs.d/elpa       --receive-keys 066DAFCB81E42C40
 sudo -u econ-ark gpg --homedir /home/econ-ark/.emacs.d/elpa/gnupg --receive-keys 066DAFCB81E42C40
 
 #Download and extract HARK, REMARK, DemARK from GitHUB repository
