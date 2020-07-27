de18692129aafabbeafcc17f4d03d089844175d6
llorracc <git@llorracc.org>
2020-07-27 12:00:24 -0400

    ISOmaker: MacOS refind script

--- Virtual/Machine/VirtualBox/ISO-maker-Server/refind-install-MacOS.sh
+++ Virtual/Machine/VirtualBox/ISO-maker-Server/refind-install-MacOS.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+cd /Volumes/HFS+/
+
 wget https://sourceforge.net/projects/refind/files/latest/download/0.12.0/refind-bin-0.12.0.zip
 
+unzip refind-bin-0.12.0.zip
+
+cd refind-bin-0.12.0
+
+sudo ./refind-install --ownhfs /dev/disk0s2
+sudo bless --setBoot --folder /Volumes/HFS+/System/Library/CoreServices --file /Volumes/HFS+/System/Library/CoreServices/refind_x64.efi --label "reFind-HFS+" --shortform
+sudo reboot
