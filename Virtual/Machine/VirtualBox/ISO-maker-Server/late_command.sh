d389f3c503429e3bfa154955663785012141dd94
llorracc <git@llorracc.org>
2020-08-04 19:21:07 -0400

    Update-Late-Command

--- Virtual/Machine/VirtualBox/ISO-maker-Server/late_command.sh
+++ Virtual/Machine/VirtualBox/ISO-maker-Server/late_command.sh
@@ -12,9 +12,6 @@ chroot /target curl -L -o /var/local/late_command https://raw.githubusercontent.
      chroot /target chmod +x /var/local/finish.sh 
      chroot /target chmod +x /var/local/refind-install-MacOS.sh 
      chroot /target chmod +x /etc/rc.local 
-     chroot /target mkdir -p /etc/lightdm/lightdm.conf.d 
-     chroot /target curl -L -o /etc/lightdm/lightdm.conf.d/autologin-econ-ark.conf https://raw.githubusercontent.com/econ-ark/econ-ark-tools/master/Virtual/Machine/VirtualBox/ISO-maker-Server/root/etc/lightdm/lightdm.conf.d/autologin-econ-ark.conf 
-     chroot /target chmod 755  /etc/lightdm/lightdm.conf.d/autologin-econ-ark.conf 
      chroot /target curl -L -o /var/local/bash_aliases-add https://raw.githubusercontent.com/econ-ark/econ-ark-tools/master/Virtual/Machine/VirtualBox/ISO-maker-Server/bash_aliases-add 
      chroot /target grub-mkconfig -o /boot/grub/grub.cfg 
 
