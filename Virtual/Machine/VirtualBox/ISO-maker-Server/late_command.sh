ff8a7a3cbc101531d4672fa6600f791a4c23ba5a
llorracc <git@llorracc.org>
2020-08-07 09:48:29 -0400

    ISOmaker-Update-Late-Command

--- Virtual/Machine/VirtualBox/ISO-maker-Server/late_command.sh
+++ Virtual/Machine/VirtualBox/ISO-maker-Server/late_command.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 curl -L -o /var/local/start.sh  https://raw.githubusercontent.com/econ-ark/econ-ark-tools/master/Virtual/Machine/VirtualBox/ISO-maker-Server/start.sh 
 curl -L -o /var/local/finish.sh https://raw.githubusercontent.com/econ-ark/econ-ark-tools/master/Virtual/Machine/VirtualBox/ISO-maker-Server/finish.sh 
+curl -L -o /var/local/finish.sh https://raw.githubusercontent.com/econ-ark/econ-ark-tools/master/Virtual/Machine/VirtualBox/ISO-maker-Server/finish-MAX-Extras.sh 
 curl -L -o /etc/rc.local   https://raw.githubusercontent.com/econ-ark/econ-ark-tools/master/Virtual/Machine/VirtualBox/ISO-maker-Server/rc.local 
 chmod +x /var/local/start.sh /var/local/finish.sh /etc/rc.local 
 mkdir -p   /usr/share/lightdm/lightdm.conf.d 
