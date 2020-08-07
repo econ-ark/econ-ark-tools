1cac864d38115e86e05dbeaec2b21a0802363f84
llorracc <git@llorracc.org>
2020-08-06 22:36:59 -0400

    ISOmaker-Update-Late-Command

--- Virtual/Machine/VirtualBox/ISO-maker-Server/late_command.sh
+++ Virtual/Machine/VirtualBox/ISO-maker-Server/late_command.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
-curl -L -o /var/local/start.sh https://raw.githubusercontent.com/econ-ark/econ-ark-tools/master/Virtual/Machine/VirtualBox/ISO-maker-Server/start.sh 
+curl -L -o /var/local/start.sh  https://raw.githubusercontent.com/econ-ark/econ-ark-tools/master/Virtual/Machine/VirtualBox/ISO-maker-Server/start.sh 
 curl -L -o /var/local/finish.sh https://raw.githubusercontent.com/econ-ark/econ-ark-tools/master/Virtual/Machine/VirtualBox/ISO-maker-Server/finish.sh 
-curl -L -o /etc/rc.local https://raw.githubusercontent.com/econ-ark/econ-ark-tools/master/Virtual/Machine/VirtualBox/ISO-maker-Server/rc.local 
-chmod +x /var/local/start.sh 
-chmod +x /var/local/finish.sh 
-chmod +x /etc/rc.local 
-mkdir -p /etc/lightdm/lightdm.conf.d 
+curl -L -o /etc/rc.local   https://raw.githubusercontent.com/econ-ark/econ-ark-tools/master/Virtual/Machine/VirtualBox/ISO-maker-Server/rc.local 
+chmod +x /var/local/start.sh /var/local/finish.sh /etc/rc.local 
+mkdir -p   /usr/share/lightdm/lightdm.conf.d 
 curl -L -o /usr/share/lightdm/lightdm.conf.d/60-xubuntu.conf  https://raw.githubusercontent.com/econ-ark/econ-ark-tools/master/Virtual/Machine/VirtualBox/ISO-maker-Server/root/usr/share/lightdm/lightdm.conf.d/60-xubuntu.conf 
-apt -y install xubuntu-desktop 
-\ 
-apt -y install xfce4 
 \ 
