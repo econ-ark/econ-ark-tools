61c1c17612435b6cfc0e0ff5d28d4b0f629897c3
llorracc <git@llorracc.org>
2020-08-10 16:57:03 -0400

    ISOmaker-Update-Late-Command

--- Virtual/Machine/VirtualBox/ISO-maker/Files/For-Target/late_command.sh
+++ Virtual/Machine/VirtualBox/ISO-maker/Files/For-Target/late_command.sh
@@ -6,6 +6,6 @@
  wget -O /var/local/finish-MAX-Extras.sh https://raw.githubusercontent.com/econ-ark/econ-ark-tools/master/Virtual/Machine/VirtualBox/ISO-maker/Files/For-Target/finish-MAX-Extras.sh 
  touch /var/local/ 
  mkdir -p /usr/share/lightdm/lightdm.conf.d /etc/systemd/system/getty@tty1.service.d 
+ wget -O /etc/systemd/system/getty@tty1.service.d/override.conf https://raw.githubusercontent.com/econ-ark/econ-ark-tools/master/Virtual/Machine/VirtualBox/ISO-maker/Files/For-Target/root/etc/systemd/system/getty@tty1.service.d/override.conf 
  wget -O /usr/share/lightdm/lightdm.conf.d/60-xubuntu.conf https://raw.githubusercontent.com/econ-ark/econ-ark-tools/master/Virtual/Machine/VirtualBox/ISO-maker/Files/For-Target/root/usr/share/lightdm/lightdm.conf.d/60-xubuntu.conf 
- chmod 755 /usr/share/lightdm/lightdm.conf.d/60-xubuntu.conf /etc/systemd/system/getty@tty1.service.d/override.conf 
- sudo apt -y install xubuntu-desktop^ xfce4 lightdm
+ chmod 755 /usr/share/lightdm/lightdm.conf.d/60-xubuntu.conf /etc/systemd/system/getty@tty1.service.d/override.conf
