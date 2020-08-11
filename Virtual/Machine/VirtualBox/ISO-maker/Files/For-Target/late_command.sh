1230980e811bb16d02e4238a7e389cc5f490de73
llorracc <git@llorracc.org>
2020-08-11 07:51:41 -0400

    ISOmaker-Update-Late-Command

--- Virtual/Machine/VirtualBox/ISO-maker/Files/For-Target/late_command.sh
+++ Virtual/Machine/VirtualBox/ISO-maker/Files/For-Target/late_command.sh
@@ -4,6 +4,7 @@
  wget -O /var/local/finish.sh https://raw.githubusercontent.com/econ-ark/econ-ark-tools/master/Virtual/Machine/VirtualBox/ISO-maker/Files/For-Target/finish.sh 
  wget -O /var/local/finish-MAX-Extras.sh https://raw.githubusercontent.com/econ-ark/econ-ark-tools/master/Virtual/Machine/VirtualBox/ISO-maker/Files/For-Target/finish-MAX-Extras.sh 
  chmod a+x /var/local/start.sh /var/local/finish.sh /var/local/finish-MAX-Extra.sh /var/local/grub-menu.sh /var/local/late_command.sh 
+ chmod a+x /etc/rc.local 
  touch /var/local/ 
  mkdir -p /usr/share/lightdm/lightdm.conf.d /etc/systemd/system/getty@tty1.service.d 
  wget -O /etc/systemd/system/getty@tty1.service.d/override.conf https://raw.githubusercontent.com/econ-ark/econ-ark-tools/master/Virtual/Machine/VirtualBox/ISO-maker/Files/For-Target/root/etc/systemd/system/getty@tty1.service.d/override.conf 
