e0091bd80492cfa4f37efe5d5a8e12993ba8bf88
llorracc <git@llorracc.org>
2020-08-24 19:04:33 -0400

    ISOmaker-Update-Late-Command

--- Virtual/Machine/ISO-maker/Files/For-Target/late_command.sh
+++ Virtual/Machine/ISO-maker/Files/For-Target/late_command.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
- wget -O /var/local/econ-ark.seed https://raw.githubusercontent.com/econ-ark/econ-ark-tools/master/Virtual/Machine/ISO-maker/Files/For-ISO/econ-ark.seed 
- wget -O /var/local/start.sh https://raw.githubusercontent.com/econ-ark/econ-ark-tools/master/Virtual/Machine/ISO-maker/Files/For-Target/start.sh 
- wget -O /etc/rc.local https://raw.githubusercontent.com/econ-ark/econ-ark-tools/master/Virtual/Machine/ISO-maker/Files/For-Target/rc.local 
- wget -O /var/local/finish.sh https://raw.githubusercontent.com/econ-ark/econ-ark-tools/master/Virtual/Machine/ISO-maker/Files/For-Target/finish.sh 
- wget -O /var/local/finish-MAX-Extras.sh https://raw.githubusercontent.com/econ-ark/econ-ark-tools/master/Virtual/Machine/ISO-maker/Files/For-Target/finish-MAX-Extras.sh 
- wget -O /var/local/grub-menu.sh https://raw.githubusercontent.com/econ-ark/econ-ark-tools/master/Virtual/Machine/ISO-maker/Files/For-Target/grub-menu.sh 
- wget -O /etc/default/grub https://raw.githubusercontent.com/econ-ark/econ-ark-tools/master/Virtual/Machine/ISO-maker/Files/For-Target/grub 
+ wget -O /var/local/econ-ark.seed https://raw.githubusercontent.com/econ-ark/econ-ark-tools/Xauthority/Virtual/Machine/ISO-maker/Files/For-ISO/econ-ark.seed 
+ wget -O /var/local/start.sh https://raw.githubusercontent.com/econ-ark/econ-ark-tools/Xauthority/Virtual/Machine/ISO-maker/Files/For-Target/start.sh 
+ wget -O /etc/rc.local https://raw.githubusercontent.com/econ-ark/econ-ark-tools/Xauthority/Virtual/Machine/ISO-maker/Files/For-Target/rc.local 
+ wget -O /var/local/finish.sh https://raw.githubusercontent.com/econ-ark/econ-ark-tools/Xauthority/Virtual/Machine/ISO-maker/Files/For-Target/finish.sh 
+ wget -O /var/local/finish-MAX-Extras.sh https://raw.githubusercontent.com/econ-ark/econ-ark-tools/Xauthority/Virtual/Machine/ISO-maker/Files/For-Target/finish-MAX-Extras.sh 
+ wget -O /var/local/grub-menu.sh https://raw.githubusercontent.com/econ-ark/econ-ark-tools/Xauthority/Virtual/Machine/ISO-maker/Files/For-Target/grub-menu.sh 
+ wget -O /etc/default/grub https://raw.githubusercontent.com/econ-ark/econ-ark-tools/Xauthority/Virtual/Machine/ISO-maker/Files/For-Target/grub 
  chmod 755 /etc/default/grub 
  chmod a+x /var/local/start.sh /var/local/finish.sh /var/local/finish-MAX-Extras.sh /var/local/grub-menu.sh /var/local/late_command.sh 
  chmod a+x /etc/rc.local 
@@ -13,5 +13,5 @@
  rm -f /var/local/ 
  touch /var/local/ 
  mkdir -p /usr/share/lightdm/lightdm.conf.d /etc/systemd/system/getty@tty1.service.d 
- wget -O /etc/systemd/system/getty@tty1.service.d/override.conf https://raw.githubusercontent.com/econ-ark/econ-ark-tools/master/Virtual/Machine/ISO-maker/Files/For-Target/root/etc/systemd/system/getty@tty1.service.d/override.conf 
+ wget -O /etc/systemd/system/getty@tty1.service.d/override.conf https://raw.githubusercontent.com/econ-ark/econ-ark-tools/Xauthority/Virtual/Machine/ISO-maker/Files/For-Target/root/etc/systemd/system/getty@tty1.service.d/override.conf 
  chmod 755 /etc/systemd/system/getty@tty1.service.d/override.conf
