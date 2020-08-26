ca42bb99d48b7797db55a149bbc470c52ebdfa0f
llorracc <git@llorracc.org>
2020-08-26 14:44:11 -0400

    ISOmaker: Fix instructions for redoing the install to the beginning of start.sh

--- Virtual/Machine/ISO-maker/Files/For-Target/start.sh
+++ Virtual/Machine/ISO-maker/Files/For-Target/start.sh
@@ -6,7 +6,7 @@
 
 rm /var/local/finished-software-install
 # To redo the whole installation sequence (without having to redownload anything):
-# sudo '(rm -f /var/local/finished-software-install ; rm -f /var/log/firstboot.log ; rm -f /var/log/secondboot.log ; rm -f /home/econ-ark/.firstboot ; rm -f /home/econ-ark/.secondboot)' >/dev/null
+# sudo bash -c '(rm -f /var/local/finished-software-install ; rm -f /var/log/firstboot.log ; rm -f /var/log/secondboot.log ; rm -f /home/econ-ark/.firstboot ; rm -f /home/econ-ark/.secondboot)' >/dev/null
 
 # define convenient "download" function
 # courtesy of http://fitnr.com/showing-file-download-progress-using-wget.html
