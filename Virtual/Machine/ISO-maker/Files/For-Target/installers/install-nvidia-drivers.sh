27821f224e521098f901e2b2a87548753f3b2aec
llorracc <git@llorracc.org>
2020-11-23 14:36:36 -0500

    Add nvidia drivers install instructions to installers

---
+++ Virtual/Machine/ISO-maker/Files/For-Target/installers/install-nvidia-drivers.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# https://linuxize.com/post/how-to-nvidia-drivers-on-ubuntu-20-04/#installing-the-nvidia-drivers-using-the-command-line
+
+ubuntu-drivers device
+
+# Find the "driver" with nvidia in the name
+# e.g. for a MacBook 2013, the command is:
+
+# sudo apt -y install nvidia-340 
+
+
+
