914c1dff1292855817774d4d9f26ca4b95394497
llorracc <git@llorracc.org>
2021-01-31 22:46:13 -0500

    Fix reversal of Anaconda archive ordering

--- Virtual/Machine/ISO-maker/Files/For-Target/finish-MAX-Extras.sh
+++ Virtual/Machine/ISO-maker/Files/For-Target/finish-MAX-Extras.sh
@@ -17,7 +17,7 @@ else
     # Stepwise filtering of the html at $CONTREPO
     # Get the topmost line that matches our requirements, extract the file name
     # (this gives us the latest stable version)
-    ANACONDAURL=$(wget -q -O - $CONTREPO index.html | grep "Anaconda3-" | grep "Linux" | grep "86_64" | head -n 1 | cut -d \" -f 2)
+    ANACONDAURL=$(wget -q -O - $CONTREPO index.html | grep "Anaconda3-" | grep "Linux" | grep "86_64" | tail -n 1 | cut -d \" -f 2)
     cmd="wget -O /tmp/Anaconda/$ANACONDAURL $CONTREPO$ANACONDAURL ; cd /tmp/Anaconda"
     echo "$cmd" # tell
     eval "$cmd" # do 
