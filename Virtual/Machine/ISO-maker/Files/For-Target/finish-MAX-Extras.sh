d3277fdb4c7bec7f1cd81de98a48e168a8b8d26a
llorracc <git@llorracc.org>
2021-02-02 21:38:34 -0500

    Update to use Anaconda3 that defaults to python 3.8

--- Virtual/Machine/ISO-maker/Files/For-Target/finish-MAX-Extras.sh
+++ Virtual/Machine/ISO-maker/Files/For-Target/finish-MAX-Extras.sh
@@ -1,28 +1,31 @@
 #!/bin/bash
-# Add extra stuff that, together, constitutes a well-provisioned scientific computing environment
-# If you have constructed the MIN machine, you should be able to upgrade it to this one
-# by running ./finish-MAX-Extras.sh
+# Add extra stuff that, together, constitutes a well-provisioned
+# scientific computing environment. If you have constructed the MIN
+# machine, you should be able to upgrade it to this one by running
+# ./finish-MAX-Extras.sh
 
 set -x # Debug
 set -v # Debug
 
 if [[ "$(which conda)" == "/usr/local/anaconda3/bin/conda" ]] ; then # It's already installed
-    conda update --yes --all conda    # It's installed, so just update it
-    conda update --yes --all anaconda # It's installed, so just update it
+    sudo conda update --yes --all conda    # It's installed, so just update it
+    sudo conda update --yes --all anaconda # It's installed, so just update it
 else
     # Put Anaconda in /tmp directory
     [[ -e /tmp/Anaconda ]] && sudo rm -Rf /tmp/Anaconda # delete any prior install
     mkdir /tmp/Anaconda ; cd /tmp/Anaconda
-    CONTREPO=https://repo.continuum.io/archive/
+    CONTREPO=https://repo.continuum.io/archive
     # Stepwise filtering of the html at $CONTREPO
     # Get the topmost line that matches our requirements, extract the file name
     # (this gives us the latest stable version)
-    ANACONDAURL=$(wget -q -O - $CONTREPO index.html | grep "Anaconda3-" | grep "Linux" | grep "86_64" | tail -n 1 | cut -d \" -f 2)
-    cmd="wget -O /tmp/Anaconda/$ANACONDAURL $CONTREPO$ANACONDAURL ; cd /tmp/Anaconda"
+    #    ANACONDAURL=$(wget -q -O - $CONTREPO index.html | grep "Anaconda3-" | grep "Linux" | grep "86_64" | tail -n 1 | cut -d \" -f 2)
+    LATEST="Anaconda3-2020.11-Linux-x86_64.sh"
+    #    ANACONDAURL="$CONTREPO$LATEST"
+    cmd="wget -O /tmp/Anaconda/$LATEST $CONTREPO/$LATEST ; cd /tmp/Anaconda"
     echo "$cmd" # tell
     eval "$cmd" # do 
 
-    cmd="sudo rm -Rf /usr/local/anaconda3 ; chmod a+x /tmp/Anaconda/$ANACONDAURL ; /tmp/Anaconda/$ANACONDAURL -b -p /usr/local/anaconda3"
+    cmd="sudo rm -Rf /usr/local/anaconda3 ; chmod a+x /tmp/Anaconda/$LATEST ; /tmp/Anaconda/$LATEST -b -p /usr/local/anaconda3"
     echo "$cmd"
     eval "$cmd"
 
@@ -45,28 +48,48 @@ else
     fi
     source /etc/environment  # Get the new environment
 
-    # Add some final common tools
-    conda install --yes -c anaconda scipy
-    conda install --yes -c anaconda pyopengl # Otherwise you get an error "Segmentation fault (core dumped)" on some Ubuntu machines
-    conda install --yes -c conda-forge jupyter_contrib_nbextensions
+    pushd . ; cd /usr/local/anaconda3 ; sudo find . -type f -iname ".sh" -exec chmod a+x {} \;
+    popd 
+    #    sudo conda install --yes -c conda-forge mamba
+    #    sudo conda create --name base_py37   --clone base 
+    #    sudo conda create --name anaconda_37 --name anaconda # Make the anaconda standard 
 
-    sudo pip install nbval
+    #    mamba update --yes --name base      conda    python=3.8
+    sudo conda install -c conda-forge mamba 
+    sudo mamba create --yes --name base_py38 python=3.8 anaconda
+    sudo conda activate base_py38
+    sudo conda install -c conda-forge mamba
+    # As of 20210202 py39 is not ready yet
+#    sudo conda create --yes --name base_py39 python=3.9 anaconda
+#    sudo conda activate base_py39
+#    sudo conda install -c conda-forge mamba
+    sudo mamba update anaconda
+    sudo conda activate base 
+    
+    # Add some final common tools
+    sudo mamba install --yes -c anaconda scipy
+    sudo mamba install --yes -c anaconda pyopengl # Otherwise you get an error "Segmentation fault (core dumped)" on some Ubuntu machines
+    sudo mamba install --yes -c conda-forge jupyter_contrib_nbextensions
+    # Make sure commands are executable by regular users
+
+    sudo conda init
+
     # Get default packages for Econ-ARK machine
     sudo apt -y install cifs-utils nautilus-share
     # Extra packages for MAX
     sudo apt -y install evince texlive-full
-
-    # Allow evince to ask the filesystem to open urls
-    # (not sure how to do this)
-    pip install quantecon
-
     sudo apt -y install /var/local/zoom_amd64.deb
 fi
 
-conda install --yes -c conda-forge econ-ark # pip install econ-ark
+sudo conda install --yes -c conda-forge econ-ark # pip install econ-ark
 
 source /etc/environment  # Get the new environment
 
+# Instructions from Anaconda mothership say to install pip stuff
+# only after installing all conda stuff
+
+sudo pip install nbval
+sudo pip install quantecon
 
 # Get docker 
 sudo apt -y remove  man-db # As of 2020-08-16, install of docker freezes at man-db step
