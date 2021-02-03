e4b14d523fc2e94dbd4d3d4ebe65e3fe72d26afb
llorracc <git@llorracc.org>
2021-02-03 11:08:35 -0500

    Update rclone info for making ISOs

--- Virtual/Machine/ISO-maker/send-both-To-Google.sh
+++ Virtual/Machine/ISO-maker/send-both-To-Google.sh
@@ -2,6 +2,27 @@
 
 pathToScript=$(dirname `realpath "$0"`)
 
+if [[ ! -e /root/.config/rclone/rclone.conf ]]; then
+    echo 'For this script to work, /root/.config/rclone/rclone.conf must exist'
+    echo 'and be configured with the right tokens for login and authentication'
+    echo 'to the target Google Drive account.'
+    echo ''
+    echo 'An example rclone.conf file that links to the econ-ark@jhuecon.org drive is at'
+    echo ''
+    echo '/var/local/root/.config/rcloneconf.zip'
+    echo ''
+    echo 'which can be unzipped using the econ-ark@jhuecon.org login password:'
+    echo ''
+    echo 'sudo mkdir -p /root/.config/rclone ; cd /var/local/root/.config/rclone ; sudo unzip /root/.config/rclone/rcloneconf.zip'
+    echo ''
+    echo 'then '
+    echo ''
+    echo 'sudo cp /var/local/root/.config/rclone/rclone.conf /root/.config/rclone'
+    echo ''
+    sleep 10
+    exit
+fi
+
 echo ''
 echo "/tmp/rclone-to-Google-Drive_Last-ISO-Made-MIN.sh:"
 [[ -e /tmp/rclone-to-Google-Drive_Last-ISO-Made-MIN.sh ]] && sudo /tmp/rclone-to-Google-Drive_Last-ISO-Made-MIN.sh
