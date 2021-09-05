cb239e60bb61b51b12aeb1ff740ba25fcf769059
llorracc <git@llorracc.org>
2021-09-05 09:44:14 -0400

    Update anaconda

--- Virtual/Machine/ISO-maker/Files/For-Target/finish-MAX-Extras-Update.sh
+++ Virtual/Machine/ISO-maker/Files/For-Target/finish-MAX-Extras-Update.sh
@@ -9,3 +9,11 @@
 
 
 
+sudo conda install anaconda=2021.05
+sudo env --name base_2021_05_fresh --clone base
+
+sudo apt-get install ripgrep fd-find
+
+# cd ~/.emacs.d/
+# git clone https://github.com/hlissner/doom-emacs
+# ~/.emacs.d/bin/doom/install
