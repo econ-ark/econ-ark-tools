a77d3509c6e268b9fef518a17dc4b08bbcc68e2f
llorracc <git@llorracc.org>
2022-04-30 11:24:25 -0400

    new late_command to download via git

---
+++ Virtual/Machine/ISO-maker/Files/For-Target/late_command_new.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+mount --bind /dev /target/dev 
+mount --bind /dev/pts /target/dev/pts 
+mount --bind /proc /target/proc 
+mount --bind /sys /target/sys 
+mount --bind /run /target/run
+sudo apt -y update
+sudo apt -y install git
+sudo mkdir -p /usr/local/share/data/GitHub/econ-ark
+sudo mkdir /var/local
+sudo chmod -Rf a+rwx /usr/local/share/data
+cd /usr/local/share/data/GitHub/econ-ark
+git clone https://github.com/econ-ark/econ-ark-tools
+cp -r Virtual/Machine/ISO-maker/Files/For-Target /var/local
+cd /var/local
+mv /etc/rc.local /etc/rc.local_orig
+mv rc.local /etc/rc.local
+ln -s /etc/rc.local 
+mv grub /etc/default/grub
+ln -s /etc/default/grub
