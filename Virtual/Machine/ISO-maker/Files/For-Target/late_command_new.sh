035d646c06c9325796fe75daa14839e361de3029
llorracc <git@llorracc.org>
2022-04-30 11:41:55 -0400

    Fix bugs in late_command_new

--- Virtual/Machine/ISO-maker/Files/For-Target/late_command_new.sh
+++ Virtual/Machine/ISO-maker/Files/For-Target/late_command_new.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 sudo apt -y update
 sudo apt -y install git
-sudo mkdir -p /usr/local/share/data/GitHub/econ-ark
-sudo mkdir /var/local
+sudo mkdir -p /usr/local/share/data/GitHub/econ-ark /var/local
 sudo chmod -Rf a+rwx /usr/local/share/data
 cd /usr/local/share/data/GitHub/econ-ark
 git clone https://github.com/econ-ark/econ-ark-tools
-cp -r Virtual/Machine/ISO-maker/Files/For-Target /var/local
+cd econ-ark-tools
+cp -r Virtual/Machine/ISO-maker/Files/For-Target/* /var/local
 cd /var/local
 mv /etc/rc.local /etc/rc.local_orig
 mv rc.local /etc/rc.local
