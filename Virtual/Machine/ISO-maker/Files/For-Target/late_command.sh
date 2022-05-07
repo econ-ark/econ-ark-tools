e77d0731b0c7ffe5dd80e00ee5833f06d2613459
llorracc <git@llorracc.org>
2022-05-07 13:01:25 -0400

    grub 2nd option now does not remove ata1

--- Virtual/Machine/ISO-maker/Files/For-Target/late_command.sh
+++ Virtual/Machine/ISO-maker/Files/For-Target/late_command.sh
@@ -8,7 +8,7 @@
  chmod -Rf a+rwx /usr/local/share/data 
  [[ ! -e /usr/local/share/data/GitHub/econ-ark/econ-ark-tools ]] && git clone --depth 1 https://github.com/econ-ark/econ-ark-tools /usr/local/share/data/GitHub/econ-ark/econ-ark-tools 
  /bin/bash -c "cd /usr/local/share/data/GitHub/econ-ark/econ-ark-tools 
- git checkout Make-ISO-Installer 
+ git checkout 
  git pull" 
  [[ -e /var/local ]] && rm -Rf /var/local 
  cp -R /usr/local/share/data/GitHub/econ-ark/econ-ark-tools/Virtual/Machine/ISO-maker/Files/For-Target /var/local 
@@ -25,15 +25,4 @@
  chmod a+x /var/local/start.sh /var/local/finish.sh /var/local/finish-MAX-Extras.sh /var/local/grub-menu.sh /var/local/late_command.sh /etc/rc.local 
  rm -f /var/local/Size-To-Make 
  rm -f /var/local/Size-To-Make 
- touch /var/local/Size-To-Make 
- mkdir -p /usr/share/lightdm/lightdm.conf.d /etc/systemd/system/getty@tty1.service.d 
- cp /var/local/root/etc/systemd/system/getty@tty1.service.d/override.conf /etc/systemd/system/getty@tty1.service.d/override.conf 
- chmod 755 /etc/systemd/system/getty@tty1.service.d/override.conf 
- apt-get --yes purge shim 
- apt-get --yes purge mokutil 
- sed -i 's/COMPRESS=lz4/COMPRESS=gzip/g' /etc/initramfs-tools/initramfs.conf 
- update-initramfs -v -c -k all 
- in-target apt-get purge -y virtualbox-guest* 
- echo grub-install --verbose --force --efi-directory=/boot/efi/ --removable --no-uefi-secure-boot --target=x86_64-efi > /var/local/grub-install-test.sh 
- grub-install --verbose --force --efi-directory=/boot/efi/ --removable --no-uefi-secure-boot --target=x86_64-efi 
- update-grub
+ touch /var/local/Size-To-Make
