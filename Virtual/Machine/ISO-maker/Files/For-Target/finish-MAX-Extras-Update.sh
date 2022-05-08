d003679f198339ba65d347bfee3ca16982a6a3b2
llorracc <git@llorracc.org>
2022-05-08 12:44:20 -0400

    Move MAX-Extras stuff into regular finish

--- Virtual/Machine/ISO-maker/Files/For-Target/finish-MAX-Extras-Update.sh
+++ Virtual/Machine/ISO-maker/Files/For-Target/finish-MAX-Extras-Update.sh
@@ -2,7 +2,7 @@
 # This script updates a machine made by the ARKINSTALL installer
 # to incorporate changes to the "master" install made since the
 # local machine's install
-# Date of install: 2021-02-02
+# Date of install: 
 # Date of updater: 
 
 # Put updates below
