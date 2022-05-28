75ced6e0c3b99d685c25d897ee73487302078420
llorracc <git@llorracc.org>
2022-05-28 19:07:21 -0400

    Version using mbpro live T2 from linuxT2

--- Virtual/Machine/ISO-maker/Files/For-Target/late_command.sh
+++ Virtual/Machine/ISO-maker/Files/For-Target/late_command.sh
@@ -32,4 +32,5 @@ mkdir /installer
 cp /var/local/Disk/Labels/Econ-ARK.disk_label /Econ-ARK.disk_label 
 cp /var/local/Disk/Labels/Econ-ARK.disk_label_2x /Econ-ARK.disk_label_2x 
 cp /var/local/Disk/Icons/Econ-ARK.VolumeIcon.icns /Econ-ARK.VolumeIcon.icns 
+update-grub 
 echo Econ-ARK > /.disk_label.contentDetails
