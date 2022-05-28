aac1dc63f747105f16280b12ca800fa459f860ae
llorracc <git@llorracc.org>
2022-05-28 12:57:40 -0400

    fix grow-me and shrink-me instructions

--- Virtual/Machine/ISO-maker/Files/For-Target/xfce-hidpi-scaling-command-grow-me.sh
+++ Virtual/Machine/ISO-maker/Files/For-Target/xfce-hidpi-scaling-command-grow-me.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 # Google xfce-hidpi-scaling-command.sh
+# Run this with bash -u so it will get the environment variables
 # 2 times scaling
+
 xfconf-query --channel xsettings --property /Gdk/WindowScalingFactor --set 2 && xfconf-query --channel xfwm4 --property /general/theme --set Default-xhdpi && xfconf-query --channel xsettings --property /Gtk/CursorThemeSize --set 48 && xfce4-panel -r
--- Virtual/Machine/ISO-maker/Files/For-Target/xfce-hidpi-scaling-command-shrink-me.sh
+++ Virtual/Machine/ISO-maker/Files/For-Target/xfce-hidpi-scaling-command-shrink-me.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Google xfce-hidpi-scaling-command.sh
+# Run this with bash -u so it will get the environment variables
 # normal scaling
-# normal scaling
+
 xfconf-query --channel xsettings --property /Gdk/WindowScalingFactor --set 1 && xfconf-query --channel xfwm4 --property /general/theme --set Default && xfconf-query --channel xsettings --property /Gtk/CursorThemeSize --set 16 && xfce4-panel -r
 
