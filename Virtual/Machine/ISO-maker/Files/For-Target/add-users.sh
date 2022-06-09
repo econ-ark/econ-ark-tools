b8842b246b0b8a1b4588ce8d2a5c066661469fb9
llorracc <git@llorracc.org>
2022-06-09 12:10:25 -0400

    More debug of add-users.sh

--- Virtual/Machine/ISO-maker/Files/For-Target/add-users.sh
+++ Virtual/Machine/ISO-maker/Files/For-Target/add-users.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-grouplist="adm,admin,dialout,cdrom,floppy,audio,dip,video,plugdev,netdev,lxd,sudo,lightdm"
+grouplist="adm,admin,dialout,cdrom,floppy,audio,dip,video,plugdev,netdev,lxd,sudo,lightdm,econ-ark"
 
 # stackoverflow.com/questions check-whether-a-user-exists
 if ! id "ubuntu" &>/dev/null; then # Probably created by seed
-    sudo useradd --create-home --password "$(perl -e 'print crypt($ARGV[0],"econ-ark")' "kra-noce")" --shell /bin/bash --groups "$grouplist" econ-ark econ-ark
+    sudo useradd --create-home --password "$(perl -e 'print crypt($ARGV[0],"econ-ark")' "kra-noce")" --shell /bin/bash --groups "$grouplist" econ-ark
 else # Probably created by multipass
-    sudo useradd --create-home --password "$(perl -e 'print crypt($ARGV[0],"econ-ark")' "kra-noce")" --shell /bin/bash --groups "$grouplist" econ-ark ubuntu
+    sudo useradd --create-home --password "$(perl -e 'print crypt($ARGV[0],"econ-ark")' "kra-noce")" --shell /bin/bash --groups "$grouplist" ubuntu
 fi
 
-sudo useradd --create-home --password "$(perl -e 'print crypt($ARGV[0],"econ-ark")' "kra-noce")" --shell /bin/bash --groups "$grouplist" econ-ark econ-ark-xrdp
+sudo useradd --create-home --password "$(perl -e 'print crypt($ARGV[0],"econ-ark")' "kra-noce")" --shell /bin/bash --groups "$grouplist" econ-ark-xrdp
 
