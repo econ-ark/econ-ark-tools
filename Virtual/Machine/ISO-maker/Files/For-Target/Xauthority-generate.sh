b47803b1156ca8e32875286407277e67dd489b32
llorracc <git@llorracc.org>
2022-06-09 17:13:03 -0400

    add Xauthority-generate.sh

---
+++ Virtual/Machine/ISO-maker/Files/For-Target/Xauthority-generate.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+hostname=$1
+myuser=$2
+
+# Get the MIT-MAGIC-COOKIE from the running instance, add the new hostname,
+magic="$(sudo xauth -f /var/run/lightdm/root/:0 list | awk '{print $NF}')"
+[[ ! -e sudo touch /root/.Xauthority ]] && sudo touch /root/.Xauthority  
+sudo xauth -vf /root/.Xauthority add $hostdate/unix:0 . "$magic"
+# Merge so that either the old or the new hostname should work
+sudo xauth -v merge /var/run/lightdm/root/:0 /root/.Xauthority
+sudo cp /root/.Xauthority /home/$myuser/.Xauthority
+# Give them the required permissions
+sudo chmod a-rwx /root/.Xauthority
+#    sudo chmod u+rw /root/.Xauthority
+# askubuntu.com/questions/253376/lightdm-failed-during-authentication
+# Says permissions should be 664 (or maybe 666)
+sudo chown $myuser:$myuser /home/$myuser/.Xauthority
+sudo chmod 664             /home/$myuser/.Xauthority
