636b2f0a50b43faf5313a4c9c84bcc1b5fc67d32
llorracc <git@llorracc.org>
2022-06-09 12:20:09 -0400

    Add comments

--- Virtual/Machine/ISO-maker/Files/For-Target/add-users.sh
+++ Virtual/Machine/ISO-maker/Files/For-Target/add-users.sh
@@ -4,12 +4,15 @@ grouplist="adm,admin,dialout,cdrom,floppy,audio,dip,video,plugdev,netdev,lxd,sud
 
 # stackoverflow.com/questions check-whether-a-user-exists
 if ! id "ubuntu" &>/dev/null; then # Probably created by seed
+    echo 'Creating user ubuntu.'
     sudo useradd --create-home --password "$(perl -e 'print crypt($ARGV[0],"econ-ark")' "kra-noce")" --shell /bin/bash --groups "$grouplist" ubuntu
 else # Probably created by multipass
-    if ! id econ-ark; then
+    if ! id econ-ark &>/dev/null; then
+	echo 'Creating user econ-ark'
 	sudo useradd --create-home --password "$(perl -e 'print crypt($ARGV[0],"econ-ark")' "kra-noce")" --shell /bin/bash --groups "$grouplist" econ-ark
     fi
 fi
 
+echo 'Creating user econ-ark-xrdp'
 sudo useradd --create-home --password "$(perl -e 'print crypt($ARGV[0],"econ-ark")' "kra-noce")" --shell /bin/bash --groups "$grouplist,econ-ark" econ-ark-xrdp
 
