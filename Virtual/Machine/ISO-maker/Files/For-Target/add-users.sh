96d1830c7b4b09e2d544e02e0f8b565064849bff
llorracc <git@llorracc.org>
2022-06-09 12:11:46 -0400

    econ-ark-xrdp is member of econ-ark

--- Virtual/Machine/ISO-maker/Files/For-Target/add-users.sh
+++ Virtual/Machine/ISO-maker/Files/For-Target/add-users.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-grouplist="adm,admin,dialout,cdrom,floppy,audio,dip,video,plugdev,netdev,lxd,sudo,lightdm,econ-ark"
+grouplist="adm,admin,dialout,cdrom,floppy,audio,dip,video,plugdev,netdev,lxd,sudo,lightdm"
 
 # stackoverflow.com/questions check-whether-a-user-exists
 if ! id "ubuntu" &>/dev/null; then # Probably created by seed
@@ -9,5 +9,5 @@ else # Probably created by multipass
     sudo useradd --create-home --password "$(perl -e 'print crypt($ARGV[0],"econ-ark")' "kra-noce")" --shell /bin/bash --groups "$grouplist" ubuntu
 fi
 
-sudo useradd --create-home --password "$(perl -e 'print crypt($ARGV[0],"econ-ark")' "kra-noce")" --shell /bin/bash --groups "$grouplist" econ-ark-xrdp
+sudo useradd --create-home --password "$(perl -e 'print crypt($ARGV[0],"econ-ark")' "kra-noce")" --shell /bin/bash --groups "$grouplist,econ-ark" econ-ark-xrdp
 
