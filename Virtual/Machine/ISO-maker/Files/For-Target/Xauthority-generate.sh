35092b40af2b8733257cbfafe971a183fc939c2a
llorracc <git@llorracc.org>
2022-06-09 17:26:43 -0400

    Another magic bug

--- Virtual/Machine/ISO-maker/Files/For-Target/Xauthority-generate.sh
+++ Virtual/Machine/ISO-maker/Files/For-Target/Xauthority-generate.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-hostname=$1
+HOST=$1
 myuser=$2
 
-# Get the MIT-MAGIC-COOKIE from the running instance, add the new hostname,
+# Get the MIT-MAGIC-COOKIE from the running instance, add the new HOST,
 magic="$(sudo xauth -f /var/run/lightdm/root/:0 list | awk '{print $NF}')"
 [[ ! -e /root/.Xauthority ]] && sudo touch /root/.Xauthority  
-sudo xauth -vf /root/.Xauthority add $hostdate/unix:0 . "$magic"
-# Merge so that either the old or the new hostname should work
+sudo xauth -vf /root/.Xauthority add $HOST/unix:0 . "$magic"
+# Merge so that either the old or the new HOST should work
 sudo xauth -v merge /var/run/lightdm/root/:0 /root/.Xauthority
 sudo cp /root/.Xauthority /home/$myuser/.Xauthority
 # Give them the required permissions
