0501cec226853be4caf29b101eb98b08e856f499
llorracc <git@llorracc.org>
2022-06-14 18:47:44 -0400

    Partway through late_command rewrite

--- Virtual/Machine/ISO-maker/Files/For-Target/late_command.sh
+++ Virtual/Machine/ISO-maker/Files/For-Target/late_command.sh
@@ -2,15 +2,14 @@
 
 set -x 
 set -v 
-apt -y update 
-apt -y install git 
+[[ '\$(which git)' ]] && apt -y reinstall git || apt -y install git 
 mkdir -p /usr/local/share/data/GitHub/econ-ark 
 chmod -Rf a+rwx /usr/local/share/data 
-[[ ! -e /usr/local/share/data/GitHub/econ-ark/econ-ark-tools ]] && sudo git clone --branch Make-Installer-ISO-WORKS https://github.com/econ-ark/econ-ark-tools /usr/local/share/data/GitHub/econ-ark/econ-ark-tools 
-sudo chmod -Rf a+rw econ-ark-tools 
+[[ ! -e /usr/local/share/data/GitHub/econ-ark/econ-ark-tools ]] && sudo git clone --depth 1 --branch Make-Installer-ISO-WORKS https://github.com/econ-ark/econ-ark-tools /usr/local/share/data/GitHub/econ-ark/econ-ark-tools 
+sudo chmod -Rf a+rw econ-ark-tools/*.[0-z]* 
 git config --global --add safe.directory /usr/local/share/data/GitHub/econ-ark/econ-ark-tools 
-[[ -e /var/local ]] && rm -Rf /var/local 
-[[ ! -e /var/local ]] && ln -s /usr/local/share/data/GitHub/econ-ark/econ-ark-tools/Virtual/Machine/ISO-maker/Files/For-Target /var/local 
+sudo chmod -Rf /usr/local/share/data/GitHub/econ-ark/econ-ark-tools/*.[0-z]* 
+[[ ! -e /var/local ]] && cp -r /usr/local/share/data/GitHub/econ-ark/econ-ark-tools/Virtual/Machine/ISO-maker/Files/For-Target /var/local 
 cd /var/local 
 [[ -e /etc/rc.local ]] && mv /etc/rc.local /etc/rc.local_orig 
 cp /var/local/rc.local /etc/rc.local 
@@ -18,7 +17,8 @@ df -hT > /tmp/target-partition
 cat /tmp/target-partition | grep '/dev' | grep -v 'loop' | grep -v 'ude' | grep -v 'tmpf' | cut -d ' ' -f1 | sed 's/.$//' > /tmp/target-dev 
 sd=$(cat /tmp/target-dev) 
 rm -f /var/local/Size-To-Make-Is-* 
-touch /var/local/Size-To-Make-Is-$(echo MIN)
+touch /var/local/Size-To-Make-Is-$(echo MIN) 
+echo $(echo MIN > /var/local/About_This_Install/machine-size.txt)
 # mkdir -p /etc/systemd/system/getty@tty1.service.d 
 cp /var/local/Disk/Labels/Econ-ARK.disk_label /Econ-ARK.disk_label 
 cp /var/local/Disk/Labels/Econ-ARK.disk_label_2x /Econ-ARK.disk_label_2x 
