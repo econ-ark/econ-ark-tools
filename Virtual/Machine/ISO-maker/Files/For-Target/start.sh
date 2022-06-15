1ab17979183abe817ed12365dc65eb7c8f604d00
llorracc <git@llorracc.org>
2022-06-14 22:28:49 -0400

    Install gh auth tools via install-gh-cli-tools.sh

--- Virtual/Machine/ISO-maker/Files/For-Target/start.sh
+++ Virtual/Machine/ISO-maker/Files/For-Target/start.sh
@@ -82,13 +82,7 @@ if [ -e /usr/bin/xfce4-about ]; then # xfce/xubuntu is installed
 #    apt -y install xfce4-goodies
 fi    
 
-
-
-# Install gh github command line tools 
-curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
-sudo apt update
-sudo apt -y install gh
+./install-gh-cli-tools.sh
 
 # sudo apt -y install tasksel
 # sudo tasksel install standard
