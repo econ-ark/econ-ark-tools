9849cf5259f55929385f1a0e4da6043945448ce8
llorracc <git@llorracc.org>
2022-06-27 01:13:51 -0400

    restore lightdm config to start.sh

--- Virtual/Machine/ISO-maker/Files/For-Target/start.sh
+++ Virtual/Machine/ISO-maker/Files/For-Target/start.sh
@@ -94,8 +94,11 @@ if ! grep -q $myuser /home/$myuser/.bash_aliases &>/dev/null; then # Econ-ARK ad
     sudo chmod a+x /root/.bash_aliases
 fi
 
-# # Choose lightdm as display manager
-# sudo echo /usr/sbin/lightdm > /etc/X11/default-display-manager 
+# Choose lightdm as display manager
+sudo echo /usr/sbin/lightdm > /etc/X11/default-display-manager
+DEBCONF_DEBUG=.*
+DEBIAN_FRONTEND=noninteractive DEBCONF_NONINTERACTIVE_SEEN=true dpkg-reconfigure lightdm
+echo set shared/default-x-display-manager lightdm | debconf-communicate
 
 # # # Purge unneeded stuff
 # # sudo apt-get -y purge ubuntu-gnome-desktop
