5b021c459fda723fe30fc1e9395be8b28245b2dd
llorracc <git@llorracc.org>
2022-06-30 13:55:31 -0400

    Remove sleep in finish

--- Virtual/Machine/ISO-maker/Files/For-Target/finish.sh
+++ Virtual/Machine/ISO-maker/Files/For-Target/finish.sh
@@ -17,7 +17,7 @@
 # Start verbose bash logging if signaled by presence of file
 [[ -e /var/local/verbose ]] && set -x && set -v
 
-sleep 3600
+# sleep 3600
 
 # Populate About_This_Install directory with info specific to this run of the installer
 myuser="econ-ark"
