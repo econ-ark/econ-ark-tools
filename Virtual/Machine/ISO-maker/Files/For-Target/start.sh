857b02eb9cab74c46aaff92684750a7ab6dc2249
llorracc <git@llorracc.org>
2022-07-03 23:42:35 -0400

    Restore rc.local to root/etc/rc.local

--- Virtual/Machine/ISO-maker/Files/For-Target/start.sh
+++ Virtual/Machine/ISO-maker/Files/For-Target/start.sh
@@ -68,7 +68,7 @@ sudo bash -c '/var/local/install-xubuntu-desktop.sh |& tee /var/local/install-xu
 # rc.local is empty by default
 [[ ! -e /etc/rc.local ]] && touch /etc/rc.local 
 mv /etc/rc.local /etc/rc.local_orig 
-cp /var/local/rc.local /etc/rc.local
+cp /var/local/root/etc/rc.local /etc/rc.local
 
 # add this stuff to any existing ~/.bash_aliases
 if ! grep -q $myuser /home/$myuser/.bash_aliases &>/dev/null; then # Econ-ARK additions are not there yet
