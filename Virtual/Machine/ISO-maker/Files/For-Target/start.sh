e7bc1505c747144d839c88a43668dc07114d02f5
llorracc <git@llorracc.org>
2022-07-04 18:08:42 -0400

    Typo in bash_aliases creation

--- Virtual/Machine/ISO-maker/Files/For-Target/start.sh
+++ Virtual/Machine/ISO-maker/Files/For-Target/start.sh
@@ -77,7 +77,7 @@ if ! grep -q $myuser /home/$myuser/.bash_aliases &>/dev/null; then # Econ-ARK ad
     sudo chown $myuser:$myuser /home/$myuser/.bash_aliases # ensure correct ownership
 fi
 
-if ! grep -q /root/.bash_aliases &>/dev/null; then # Econ-ARK additions are not there yet
+if ! grep -q root /root/.bash_aliases &>/dev/null; then # Econ-ARK additions are not there yet
     # Same bash shell for root user
     sudo cat /var/local/root/home/user_root/bash_aliases   >> /root/.bash_aliases 
     sudo chmod a+x /root/.bash_aliases
