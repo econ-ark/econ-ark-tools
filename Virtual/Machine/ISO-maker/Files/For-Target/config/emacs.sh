fef74f5791445ef318f934b85ac01da2b083fdaa
llorracc <git@llorracc.org>
2022-07-09 19:01:14 -0400

    make config/emac.sh compatible with install-emacs.sh

--- Virtual/Machine/ISO-maker/Files/For-Target/config/emacs.sh
+++ Virtual/Machine/ISO-maker/Files/For-Target/config/emacs.sh
@@ -1,28 +1,33 @@
 #!/bin/bash
+# Assumes emacs has already been installed by ./install-emacs.sh
 
 if [[ "$#" -ne 1 ]]; then
     echo 'usage: config-emacs.sh [username]'
     exit
 fi
 
+[[ -e /var/local/status/verbose ]] && set -x && set -v
 myuser=$1
 
-[[ -e /var/local/status/verbose ]] && set -x && set -v
+shared=/usr/local/share/emacs
+myhome=/home/$myuser
 
-[[ ! -e /root/.emacs.d ]] && echo 'First run installers/install-emacs.sh to create root setup' && exit
+[[ ! -e /$shared/.emacs.d ]] && echo 'First run installers/install-emacs.sh to create root setup' && exit
 
 localhome=var/local/root/home # templates
 
+install_time="$(date +%Y%m%d%H%M)"
 ## Create .emacs files
-[[ -e /home/$myuser/.emacs ]] && mv /home/$myuser/.emacs /home/$myuser/.emacs_orig
+[[ -e /home/$myuser/.emacs ]] && mv /home/$myuser/.emacs /home/$myuser/.emacs_orig_$install_time
 rm -f /home/$myuser/.emacs
 cp /$localhome/user_regular/dotemacs-regular-users /home/$myuser/.emacs
+ln -s /$localhome/user_regular/dotemacs-regular-users /home/$myuser/.emacs_econ-ark_$(</var/local/About_This_Install/commit_hash)
 
 # Create .emacs.d directory with proper permissions -- avoids annoying startup warning msg
-[[ -e /home/$myuser/.emacs.d ]] && mv /home/$myuser/.emacs.d_orig
+[[ -e /home/$myuser/.emacs.d ]] && mv /home/$myuser/.emacs.d_$install_time
 
-# Don't install the packages twice - instead, link root to the existing install
-[[ -e /root/.emacs.d ]] && ln -s /root/.emacs.d /home/$myuser/.emacs.d 
+# Don't install packages separately for each user - instead, link root to the existing install
+[[ -e /$shared/.emacs.d ]] && ln -s /$shared/.emacs.d /home/$myuser/.emacs.d 
 
 # Do emacs first-time setup
 emacs -batch --eval "(setq debug-on-error t)" -l     /home/$myuser/.emacs  
