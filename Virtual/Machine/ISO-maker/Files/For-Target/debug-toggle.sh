460764c9cb8e24cf02c34de0818e78da8133278c
llorracc <git@llorracc.org>
2022-07-09 11:45:21 -0400

    Remove sleep; different hostname for verbose vs regular build mode

---
+++ Virtual/Machine/ISO-maker/Files/For-Target/debug-toggle.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [[ "$#" -ne 0 ]]; then
+    echo 'debug-toggle takes no arguments; it simply switches the state of the verbosity flag '
+    exit
+fi
+
+if [[ -e /var/local/status/verbose ]]; then
+    echo 'verbose debugging mode was turned on; turning off'
+    rm /var/local/status/verbose
+else
+    echo 'verbose debugging mode was turned off; turning on'
+    touch /var/local/status/verbose
+fi
