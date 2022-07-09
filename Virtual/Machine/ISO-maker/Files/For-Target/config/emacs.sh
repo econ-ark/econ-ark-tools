380c10e10f153531382a92247c29e8a29857b3c3
llorracc <git@llorracc.org>
2022-07-09 15:55:39 -0400

    Fix bugs in config/emacs.sh

--- Virtual/Machine/ISO-maker/Files/For-Target/config/emacs.sh
+++ Virtual/Machine/ISO-maker/Files/For-Target/config/emacs.sh
@@ -9,6 +9,7 @@ myuser=$1
 
 [[ -e /var/local/status/verbose ]] && set -x && set -v
 
+[[ ! -e /root/.emacs.d ]] && echo 'First run installers/install-emacs.sh to create root setup' && exit
 
 localhome=var/local/root/home # templates
 
@@ -16,17 +17,14 @@ localhome=var/local/root/home # templates
 [[ -e /home/$myuser/.emacs ]] && mv /home/$myuser/.emacs /home/$myuser/.emacs_orig
 rm -f /home/$myuser/.emacs
 cp /$localhome/user_regular/dotemacs-regular-users /home/$myuser/.emacs
-    
-# Create .emacs.d directory with proper permissions -- avoids annoying startup warning msg
-[[ ! -e /home/$myuser/.emacs.d ]] && mkdir /home/$myuser/.emacs.d && chown "$myuser:$myuser" /home/$myuser/.emacs.d
 
-# Do emacs first-time setup (including downloading packages)
-emacs -batch -l     /home/$myuser/.emacs  
+# Create .emacs.d directory with proper permissions -- avoids annoying startup warning msg
+[[ -e /home/$myuser/.emacs.d ]] && mv /home/$myuser/.emacs.d_orig
 
 # Don't install the packages twice - instead, link root to the existing install
-[[ -e /root/.emacs.d ]] && ln -s /home/$myuser/.emacs.d /root/.emacs.d
+[[ -e /root/.emacs.d ]] && ln -s /root/.emacs.d /home/$myuser/.emacs.d 
 
-# Do emacs first-time setup (including downloading packages)
+# Do emacs first-time setup
 emacs -batch --eval "(setq debug-on-error t)" -l     /home/$myuser/.emacs  
 
 # Finished with emacs
