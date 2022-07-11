8e361df72c47e83e3958102806d155c2dc2b4663
llorracc <git@llorracc.org>
2022-07-11 18:18:33 -0400

    Set default geometry for headless (VB)

--- Virtual/Machine/ISO-maker/Files/For-Target/config/xwindows.sh
+++ Virtual/Machine/ISO-maker/Files/For-Target/config/xwindows.sh
@@ -8,8 +8,3 @@ xset r off
 xfwm4compositorstatus="$(xfconf-query --channel xfwm4 --property /general/use_compositing --list)"
 xfwm4compositorstatusExists="$?"
 [[ "$xfwm4compositorstatusExists" == "0" ]] && xfconf-query --channel xfwm4 --property /general/use_compositing --type 'bool' --set false
-
-# Set default geometry
-## https://askubuntu.com/questions/453109/add-fake-display-when-no-monitor-is-plugged-in
-## https://en.wikipedia.org/wiki/Display_resolution
-xrandr --fb 1366x768 
