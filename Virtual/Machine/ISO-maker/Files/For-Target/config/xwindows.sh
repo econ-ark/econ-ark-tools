907f439c3437a9a34dfa8d912d3ac5141b7cbe65
llorracc <git@llorracc.org>
2022-07-11 15:46:56 -0400

    xrandr --fb 1366x768 in xwindows.sh sets default geometry

--- Virtual/Machine/ISO-maker/Files/For-Target/config/xwindows.sh
+++ Virtual/Machine/ISO-maker/Files/For-Target/config/xwindows.sh
@@ -9,4 +9,7 @@ xfwm4compositorstatus="$(xfconf-query --channel xfwm4 --property /general/use_co
 xfwm4compositorstatusExists="$?"
 [[ "$xfwm4compositorstatusExists" == "0" ]] && xfconf-query --channel xfwm4 --property /general/use_compositing --type 'bool' --set false
 
-
+# Set default geometry
+## https://askubuntu.com/questions/453109/add-fake-display-when-no-monitor-is-plugged-in
+## https://en.wikipedia.org/wiki/Display_resolution
+xrandr --fb 1366x768 
