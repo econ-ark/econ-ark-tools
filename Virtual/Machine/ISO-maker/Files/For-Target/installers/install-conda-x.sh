fe522d3fa8395531d8eeb90773faa07468659726
econ-ark <econ-ark@XUB20ARK.llorracc.net>
2022-07-14 11:48:02 -0400

    install-conda-x works for VBox VM

---
+++ Virtual/Machine/ISO-maker/Files/For-Target/installers/install-conda-x.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+bad=false
+
+[[ "$#" -ne 1 ]] && bad=true
+
+# In case they used capitals
+CHOSEN=$(echo $1 | tr '[:upper:]' '[:lower:]')
+# CHOSEN=miniconda
+ANA='anaconda' && MIN='miniconda'
+
+echo $CHOSEN
+[[ "$CHOSEN" != "$ANA" ]] && [[ "$CHOSEN" != "$MIN" ]] && bad=true
+
+if [[ "$bad" == true ]]; then
+    echo 'usage: install-conda-x.sh [ anaconda | miniconda ]'
+    exit
+fi
+
+# Put $CHOSEN in /tmp directory
+[[ -e /tmp/$CHOSEN ]] && sudo rm -Rf /tmp/$CHOSEN # delete any prior install
+mkdir /tmp/$CHOSEN ; cd /tmp/$CHOSEN
+
+[[ "$CHOSEN" == "$ANA" ]] && LATEST="Anaconda=3-2021.11-Linux-x86_64.sh" && URL="repo.continuum.io/archive"
+[[ "$CHOSEN" == "$MIN" ]] && LATEST="Miniconda3-py39_4.12.0-Linux-x86_64.sh" && URL="repo.anaconda.com"
+
+# (wisely) gave up on automatically retrieving latest version
+# 2021.11: Python version is 3.9
+cmd="wget         -O /tmp/$CHOSEN/$LATEST https://$URL/$CHOSEN/$LATEST ; cd /tmp/$CHOSEN"
+#cmd="wget --quiet -O /tmp/$CHOSEN/$LATEST $SOURCE/$CHOSEN/$LATEST ; cd /tmp/$CHOSEN"
+echo "$cmd" # tell
+eval "$cmd" # do
+
+# cmd="sudo rm -Rf /usr/local/$CHOSEN ; chmod a+x /tmp/$CHOSEN/$LATEST ; /tmp/$CHOSEN/$LATEST -b -p /usr/local/$CHOSEN"
+# echo "$cmd"
+# eval "$cmd"
+
+sudo rm -Rf /usr/local/$CHOSEN
+sudo chmod a+x /tmp/$CHOSEN/$LATEST
+sudo /tmp/$CHOSEN/$LATEST -b -p /usr/local/$CHOSEN
+
+# Add to default enviroment path so that all users can find it
+
+source /etc/environment
+if [[ ! "$PATH" == *"/usr/local/$CHOSEN"* ]]; then
+    echo 'Adding '$CHOSEN' to PATH'
+    # addToPath="export PATH=/usr/local/$CHOSEN/bin:$PATH"
+    # echo "$addToPath"
+    # eval "$addToPath"
+    sudo chmod u+w /etc/environment
+    sudo rm -Rf /tmp/environment
+    sudo sed -e "s\/usr/local/sbin:\/usr/local/"$CHOSEN"/bin:/usr/local/sbin:\g" /etc/environment > /tmp/environment
+    sudo mv /tmp/environment /etc/environment # Weird permissions issue prevents direct redirect into /etc/environment
+    sudo chmod u-w /etc/environment # Restore secure permissions for environment
+fi
+
+# Pull in the modified environment
+source /etc/environment  # Get the new environment
+
+[[ ! -e /etc/sudoers.d ]] && sudo mkdir -p /etc/sudoers.d && sudo chmod a+w /etc/sudoers.d
+if [[ ! -e /etc/sudoers.d/$CHOSEN ]]; then
+    sudo echo 'Defaults secure_path="/usr/local/'$CHOSEN'/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/snap/bin:/bin"' | sudo tee /etc/sudoers.d/$CHOSEN
+fi
+sudo chmod 555 /etc/sudoers.d
+
+# The sudos below are not necessary when this script is originally run
+# But they are useful when debugging it because they allow copy and paste
+# of text to a non-root shell on a line-by-line basis
+
+sudo conda init
+
+# Because installed as root, files are not executable by non-root users but should be
+pushd .
+cd /usr/local/$CHOSEN
+sudo find . -type f -iname ".sh"  -exec chmod a+x {} \;
+sudo find . -type f -iname "..sh" -exec chmod a+x {} \; # Gets csh, zsh, whatever
+popd
+
+# sudo conda install --yes -c conda-forge mamba
+sudo conda activate base
+
+# Add some final common tools
+sudo conda install --yes -c conda-forge jupyter_contrib_nbextensions
