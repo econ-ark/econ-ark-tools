9f569ea6cb61a80aa43f08cce3d6269e05a7c153
llorracc <git@llorracc.org>
2022-07-27 23:28:52 -0400

    Add apt-utils to first things installed

---
+++ Virtual/Machine/ISO-maker/Files/For-Target/config/config-timeshift-backups.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Default is for machine to back itself up upon boot and nightly
+# to the directory /timeshift
+
--- Virtual/Machine/ISO-maker/Files/For-Target/start.sh
+++ Virtual/Machine/ISO-maker/Files/For-Target/start.sh
@@ -31,7 +31,7 @@ mypass="kra-noce"       # both have the same password
 sudo systemctl mask sleep.target suspend.target hibernate.target hybrid-sleep.target
 
 # Get some basic immediately useful tools 
-sudo apt-get -y install bash-completion curl
+sudo apt-get -y install bash-completion curl  apt-utils
 
 # Create with appropriate groups
 /var/local/config/add-users.sh |& tee /var/local/status/config-users.log
