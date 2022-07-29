ae40fa9a65d132058396869fa0e0f776d12bb43a
llorracc <git@llorracc.org>
2022-07-29 11:09:00 -0400

    Add upgrade-MIN-to-MAX.sh

---
+++ Virtual/Machine/ISO-maker/Files/For-Target/upgrade-MIN-to-MAX.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Run this if you installed the MIN version
+# and want to upgrade to the MAX version
+
+cd /var/local/status
+
+if [[ ! -e /Size-To-Make-Is-MIN ]]; then
+    echo 'Size-To-Make is not currently MIN'
+    exit
+fi
+    
+sudo mv /var/local/status/Size-To-Make-Is-MIN /var/local/status/Size-To-Make-Is-MAX
+
+sudo /var/local/tools/finish-with-log.sh
+
