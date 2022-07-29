af099b39499f57f20240a21ab0694171d94d1b52
llorracc <git@llorracc.org>
2022-07-28 23:21:57 -0400

    Start and finish log fix

--- Virtual/Machine/ISO-maker/Files/For-Target/tools/finish-with-log.sh
+++ Virtual/Machine/ISO-maker/Files/For-Target/tools/finish-with-log.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-bash -c 'sudo /var/local/finish.sh |& sudo tee /var/local/status/finish.log'
+bash -c 'sudo /var/local/finish.sh |& sudo tee /var/local/status/finish.log |& sudo tee -a /var/local/status/start-and-finish.log'
