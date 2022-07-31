0d08c7e1caadbb62a3a32763b709ecc8634a4b8d
llorracc <git@llorracc.org>
2022-07-31 16:14:00 -0400

    Just edit existing timeshift config

--- Virtual/Machine/ISO-maker/Files/For-Target/config/config-timeshift-backups.sh
+++ Virtual/Machine/ISO-maker/Files/For-Target/config/config-timeshift-backups.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # timeshift makes automated backups
 
-partition_containing_sys="$(df -h / | tail -1 | cut -d ' ' -f1)"
-uuid_of_root_partition="$(sudo blkid | grep UUID= | grep "$partition_containing_sys" | cut -d ' ' -f2)"
-sed_code="s|partition_containing_sys|"$uuid_of_root_partition"|g"
+cp /etc/timeshift.json /etc/timeshift_orig.json
 
-sed -e "$sed_code" /var/local/sys_root_dir/etc/timeshift/timeshift.json > /etc/timeshift.json
+rpl  '"schedule_hourly" : "false",' '"schedule_hourly" : "true",' /etc/timeshift.json
+rpl  '"count_monthly" : "2",'       '"count_monthly" : "6",'      /etc/timeshift.json
+
 
 
