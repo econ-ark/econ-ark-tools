3b4d2034629a962cb8a2bbf7a0809dfb1a6ee463
llorracc <git@llorracc.org>
2022-07-31 12:45:22 -0400

    set up to purge locales other than en_US

--- Virtual/Machine/ISO-maker/Files/For-Target/late_command_finish.sh
+++ Virtual/Machine/ISO-maker/Files/For-Target/late_command_finish.sh
@@ -3,12 +3,16 @@
 # Verbose output if 'verbose' indicator file exists
 [[ -e /var/local/status/verbose ]] && set -x && set -v
 
+# Prevent installation of endless locale information
+sudo apt -y install localepurge
+sudo locale-gen --purge en_US en_US.UTF-8 en_US.8859-15
+
 # Figure out what the target device is 
 df -hT > /tmp/target-partition 
 cat /tmp/target-partition | grep -v vfat | grep '/dev' | grep -v 'loop' | grep -v 'ude' | grep -v 'tmpf' | cut -d ' ' -f1 | sed 's/.$//' > /tmp/target-dev 
 sd=$(cat /tmp/target-dev)
 
-# On Macs, this should allow nice icons when disk is viewed:
+# On some Macs, this should allow nice icons when disk is viewed:
 mkdir -p /EFI/BOOT/
 cp /var/local/sys_root_dir/EFI/BOOT/Econ-ARK.disk_label     /EFI/BOOT/Econ-ARK.disk_label 
 cp /var/local/sys_root_dir/EFI/BOOT/Econ-ARK.disk_label_2x  /EFI/BOOT/Econ-ARK.disk_label_2x 
