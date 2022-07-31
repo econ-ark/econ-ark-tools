4d53b2264045c034a6ca592e144148e56c35ed72
llorracc <git@llorracc.org>
2022-07-31 15:43:04 -0400

    Fix timeshift backup

--- Virtual/Machine/ISO-maker/Files/For-Target/config/config-timeshift-backups.sh
+++ Virtual/Machine/ISO-maker/Files/For-Target/config/config-timeshift-backups.sh
@@ -2,7 +2,8 @@
 # timeshift makes automated backups
 
 partition_containing_sys="$(df -h / | tail -1 | cut -d ' ' -f1)"
-sed_code="s|partition_containing_sys|"$partition_containing_sys"|g"
+uuid_of_root_partition="$(sudo blkid | grep UUID= | grep "$partition_containing_sys" | cut -d ' ' -f2)"
+sed_code="s|partition_containing_sys|"$uuid_of_root_partition"|g"
 
 sed -e "$sed_code" /var/local/sys_root_dir/etc/timeshift/timeshift.json > /etc/timeshift.json
 
--- Virtual/Machine/ISO-maker/Files/For-Target/config/config-timeshift.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# timeshift makes automated backups
-
-partition_containing_sys="$(df -h / | tail -1 | cut -d ' ' -f1)"
-sed_code="s|partition_containing_sys|"$partition_containing_sys
-
-sed -e "$sed_code" /var/local/sys_root_dir/etc/timeshift/timeshift.json > /etc/timeshift.json
-
-
