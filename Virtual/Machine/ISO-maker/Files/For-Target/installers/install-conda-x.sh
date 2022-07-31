8018e31ab023fa65de6caa9d888cd7569847d233
llorracc <git@llorracc.org>
2022-07-30 23:50:30 -0400

    Semicolons must be in quotes

--- Virtual/Machine/ISO-maker/Files/For-Target/installers/install-conda-x.sh
+++ Virtual/Machine/ISO-maker/Files/For-Target/installers/install-conda-x.sh
@@ -13,7 +13,7 @@ ANA='anaconda' && MIN='miniconda'
 
 [[ "$CHOSEN" != "$ANA" ]] && [[ "$CHOSEN" != "$MIN" ]] && bad_syntax=true
 
-[[ "$CHOSEN" == "" ]] && echo CHOSEN has no value; aborting && exit
+[[ "$CHOSEN" == "" ]] && echo "CHOSEN has no value; aborting" && exit
 
 if [[ "$bad_syntax" == true ]]; then
     echo 'usage: install-conda-x.sh [ anaconda | miniconda ]'
