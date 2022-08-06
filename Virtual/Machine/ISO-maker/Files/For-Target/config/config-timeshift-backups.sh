e0eb415a935b1c5c72a51e900edcd512d92f0e30
llorracc <git@llorracc.org>
2022-08-06 10:31:23 -0400

    fix typo

--- Virtual/Machine/ISO-maker/Files/For-Target/config/config-timeshift-backups.sh
+++ Virtual/Machine/ISO-maker/Files/For-Target/config/config-timeshift-backups.sh
@@ -7,7 +7,7 @@
 # If there's an existing config file, preserve it
 install_time="$(date +%Y%m%d%H%M)"
 [[ -e /etc/timeshift.json ]] && sudo mv /var/local/sys_root_dir/etc/timeshift.json /etc/timeshift_orig_$install_time.json
-sudo /var/local/sys_root_dir/etc/timeshift/timeshift.json /etc/timeshift/timeshift.json
+sudo cp /var/local/sys_root_dir/etc/timeshift/timeshift.json /etc/timeshift/timeshift.json
 
 # Modify the default config
 sudo rpl  '"schedule_hourly" : "false",' '"schedule_hourly" : "true",' /etc/timeshift/timeshift.json
