26e5c5b87d76fc8c8e92ce18598bec5899b65613
llorracc <git@llorracc.org>
2022-08-06 15:50:25 -0400

    Fix timeshift.json path

--- Virtual/Machine/ISO-maker/Files/For-Target/config/config-timeshift-backups.sh
+++ Virtual/Machine/ISO-maker/Files/For-Target/config/config-timeshift-backups.sh
@@ -6,7 +6,7 @@
 
 # If there's an existing config file, preserve it
 install_time="$(date +%Y%m%d%H%M)"
-[[ -e /etc/timeshift.json ]] && sudo mv /var/local/sys_root_dir/etc/timeshift.json /etc/timeshift_orig_$install_time.json
+[[ -e /etc/timeshift/timeshift.json ]] && sudo mv /etc/timeshift/timeshift.json /etc/timeshift/timeshift_orig_$install_time.json
 sudo cp /var/local/sys_root_dir/etc/timeshift/timeshift.json /etc/timeshift/timeshift.json
 
 sys_UUID="$(sudo /var/local/tools/determine-sys-UUID.sh)"
