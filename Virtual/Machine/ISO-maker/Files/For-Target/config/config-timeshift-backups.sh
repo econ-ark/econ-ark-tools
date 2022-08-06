9c02f3b9a641a0c6fc6802ab209a4e1e6e22ad77
llorracc <git@llorracc.org>
2022-08-06 10:17:42 -0400

    Fix config-timeshift-backups

--- Virtual/Machine/ISO-maker/Files/For-Target/config/config-timeshift-backups.sh
+++ Virtual/Machine/ISO-maker/Files/For-Target/config/config-timeshift-backups.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 # timeshift makes automated backups
 
-cp /etc/timeshift.json /etc/timeshift_orig.json
+# If it isn't installed yet, install it
+[[ "$(which timeshift)" == "" ]] && sudo /var/local/installers/install-timeshift.sh
 
-rpl  '"schedule_hourly" : "false",' '"schedule_hourly" : "true",' /etc/timeshift.json
-rpl  '"count_monthly" : "2",'       '"count_monthly" : "6",'      /etc/timeshift.json
+# If there's an existing config file, preserve it
+[[ -e /etc/timeshift.json ]] && sudo cp /var/local/sys_root_dir/etc/timeshift.json /etc/timeshift_orig.json
+
+# Modify the default config
+sudo rpl  '"schedule_hourly" : "false",' '"schedule_hourly" : "true",' /etc/timeshift.json
+sudo rpl  '"count_monthly" : "2",'       '"count_monthly" : "6",'      /etc/timeshift.json
 
 
 
