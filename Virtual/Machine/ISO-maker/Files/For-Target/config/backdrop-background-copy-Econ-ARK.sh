338ccc0790777279698b5998a4462c7dfd7cfb45
llorracc <git@llorracc.org>
2022-08-06 16:51:24 -0400

    Fix path for backgrounds

--- Virtual/Machine/ISO-maker/Files/For-Target/config/backdrop-background-copy-Econ-ARK.sh
+++ Virtual/Machine/ISO-maker/Files/For-Target/config/backdrop-background-copy-Econ-ARK.sh
@@ -2,10 +2,10 @@
 # Put the image in the right places
 
 backdrops=usr/share/xfce4/backdrops
-backgrounds=usr/share/backgrounds/xfce
+backgrnds=usr/share/backgrounds/xfce
 
 # Two DIFFERENT places for backdrops, depending on xubuntu-core versus xubuntu-desktop
 
-[[ ! -e   /var/local/sys_root_dir/$backdrops/Econ-ARK-Logo-1536x768.png ]] && cp /var/local/sys_root_dir/$backdrops/Econ-ARK-Logo-1536x768.*   /$backdrops
-[[ ! -e /var/local/sys_root_dir/$backgrounds/Econ-ARK-Logo-1536x768.png ]] && cp /var/local/sys_root_dir/$backgrounds/Econ-ARK-Logo-1536x768.*   /$backgrounds
+[[ ! -e /$backdrops/Econ-ARK-Logo-1536x768.png ]] && cp /var/local/sys_root_dir/$backdrops/Econ-ARK-Logo-1536x768.*   /$backdrops
+[[ ! -e /$backgrnds/Econ-ARK-Logo-1536x768.png ]] && cp /var/local/sys_root_dir/$backgrnds/Econ-ARK-Logo-1536x768.*   /$backgrnds
 
--- Virtual/Machine/ISO-maker/Files/For-Target/tools/determine-sys-UUID.sh
+++ Virtual/Machine/ISO-maker/Files/For-Target/tools/determine-sys-UUID.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 sys_part="$(/var/local/tools/determine-sys-partition.sh)"
-UUID="$(blkid | grep "$sys_part" | rev | cut -d ' ' -f1 | rev)"
-eval "$(blkid | grep "$sys_part" | rev | cut -d ' ' -f1 | rev)"
+eval "$(blkid | grep "$sys_part": | rev | cut -d ' ' -f1 | rev)"
 echo $PARTUUID
 
 
