c69ef2e1dc28b027328ea38a48eb3e141c883687
llorracc <git@llorracc.org>
2022-08-06 10:29:30 -0400

    More fixes for config-timeshift

--- Virtual/Machine/ISO-maker/Files/For-Target/config/config-timeshift-backups.sh
+++ Virtual/Machine/ISO-maker/Files/For-Target/config/config-timeshift-backups.sh
@@ -5,11 +5,13 @@
 [[ "$(which timeshift)" == "" ]] && sudo /var/local/installers/install-timeshift.sh
 
 # If there's an existing config file, preserve it
-[[ -e /etc/timeshift.json ]] && sudo cp /var/local/sys_root_dir/etc/timeshift.json /etc/timeshift_orig.json
+install_time="$(date +%Y%m%d%H%M)"
+[[ -e /etc/timeshift.json ]] && sudo mv /var/local/sys_root_dir/etc/timeshift.json /etc/timeshift_orig_$install_time.json
+sudo /var/local/sys_root_dir/etc/timeshift/timeshift.json /etc/timeshift/timeshift.json
 
 # Modify the default config
-sudo rpl  '"schedule_hourly" : "false",' '"schedule_hourly" : "true",' /etc/timeshift.json
-sudo rpl  '"count_monthly" : "2",'       '"count_monthly" : "6",'      /etc/timeshift.json
+sudo rpl  '"schedule_hourly" : "false",' '"schedule_hourly" : "true",' /etc/timeshift/timeshift.json
+sudo rpl  '"count_monthly" : "2",'       '"count_monthly" : "6",'      /etc/timeshift/timeshift.json
 
 
 
