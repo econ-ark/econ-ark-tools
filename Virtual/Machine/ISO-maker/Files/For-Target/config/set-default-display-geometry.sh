9504f793a1aefcf54ac354b7932de15ca8ef7562
llorracc <git@llorracc.org>
2022-08-07 18:01:50 -0400

    Increase default geometry

--- Virtual/Machine/ISO-maker/Files/For-Target/config/set-default-display-geometry.sh
+++ Virtual/Machine/ISO-maker/Files/For-Target/config/set-default-display-geometry.sh
@@ -5,6 +5,6 @@
 ## https://en.wikipedia.org/wiki/Display_resolution
 
 if [[ "$(which lshw)" ]] && vbox="$(lshw 2>/dev/null | grep VirtualBox)"  && [[ "$vbox" != "" ]]; then
-    xrandr --fb 1366x768
+    xrandr --fb 1600x900
 fi
 
