2de6d32b85bd01ce94f423216b29755ca6f6f8f0
llorracc <git@llorracc.org>
2022-08-08 10:39:29 -0400

    tweak partman early commad

--- Virtual/Machine/ISO-maker/Files/For-ISO/partman_early_command.sh
+++ Virtual/Machine/ISO-maker/Files/For-ISO/partman_early_command.sh
@@ -3,8 +3,8 @@
 CDDEV="$(mount | grep cdrom | cut -d' ' -f1 | sed 's/\(.*\)./\1/')" 
 USBDEV_LIST="/tmp/USBDEV_LIST" 
 list-devices usb-partition | sed "s/\(.*\)./\1/" | grep -v "$CDDEV" > "$USBDEV_LIST" 
-BOOTDEV="$(list-devices disk | grep -f "$USBDEV_LIST" | grep -v "$CDDEV" | tail -1)" 
-echo BOOTDEV="$BOOTDEV"  
+BOOTDEV_CMD="$(list-devices disk | grep -f "$USBDEV_LIST" | grep -v "$CDDEV" | tail -1)" 
+BOOTDEV="$BOOTDEV_CMD"
 echo "$BOOTDEV" > /tmp/BOOTDEV 
 if [[ ! -z "$BOOTDEV" ]]; then 
     debconf-set partman-auto/disk "$BOOTDEV" 
