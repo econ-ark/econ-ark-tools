62462673219e45d89bbd7cb8ecc1b85c9c37c282
llorracc <git@llorracc.org>
2022-08-08 13:50:27 -0400

    Fix partman detect disk

--- Virtual/Machine/ISO-maker/Files/For-ISO/partman_early_command.sh
+++ Virtual/Machine/ISO-maker/Files/For-ISO/partman_early_command.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 CDDEV="$(mount | grep cdrom | cut -d' ' -f1 | sed 's/\(.*\)./\1/')" 
 USBDEV_LIST="/tmp/USBDEV_LIST" 
@@ -6,7 +6,7 @@ list-devices usb-partition | sed "s/\(.*\)./\1/" | grep -v "$CDDEV" > "$USBDEV_L
 BOOTDEV_CMD="$(list-devices disk | grep -f "$USBDEV_LIST" | grep -v "$CDDEV" | tail -1)" 
 BOOTDEV="$BOOTDEV_CMD"
 echo "$BOOTDEV" > /tmp/BOOTDEV 
-if [[ ! -z "$BOOTDEV" ]]; then 
+if [[ ! "$BOOTDEV" == "" ]]; then 
     debconf-set partman-auto/disk "$BOOTDEV" 
     debconf-set grub-installer/bootdev "$BOOTDEV" 
 fi 
