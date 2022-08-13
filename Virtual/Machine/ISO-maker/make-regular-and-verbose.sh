4add406365d82fd4000111bb47275bf7e4145e3f
llorracc <git@llorracc.org>
2022-08-13 15:26:56 -0400

    Verbose off

--- Virtual/Machine/ISO-maker/make-regular-and-verbose.sh
+++ Virtual/Machine/ISO-maker/make-regular-and-verbose.sh
@@ -7,20 +7,19 @@ cd $pathToScript
 
 [[ -e /var/local/status/verbose ]] && mv /var/local/status/verbose /var/local/status/verbose_off
 
-git add . ; git commit -m 'Verbose on' ; git push
+git add . ; git commit -m 'Verbose off' ; git push
 
 $pathToScript/make-Internal-Allow.sh
 $pathToScript/make-Internal-Prohibit.sh
 
-if [[ -e /var/local/status/verbose ]]; then
+if [[ -e /var/local/status/verbose_off ]]; then
     mv /var/local/status/verbose_off /var/local/status/verbose
 else
     mkdir -p /var/local/status/verbose
     touch    /var/local/status/verbose/.gitkeep
 fi
 
-
-git add . ; git commit -m 'Verbose off' ; git push
+git add . ; git commit -m 'Verbose on' ; git push
 
 $pathToScript/make-Internal-Allow.sh
 $pathToScript/make-Internal-Prohibit.sh
