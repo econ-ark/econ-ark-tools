7971a1aca6fcdee1ef758aa582359f034fe40015
llorracc <git@llorracc.org>
2022-09-28 14:15:00 -0400

    add install-chrome.sh

--- Virtual/Machine/ISO-maker/Files/For-Target/finish.sh
+++ Virtual/Machine/ISO-maker/Files/For-Target/finish.sh
@@ -48,9 +48,8 @@ sudo apt -y install texlive-base texlive-latex-base texlive-extra-utils
 sudo /var/local/installers/install-emacs.sh |& tee /var/local/status/install-emacs.log
 [[ "$(which emacs)"  != "" ]] && sudo emacs -batch -eval "(setq debug-on-error t)" -l /root/.emacs
 
-# Install Chrome browser 
-wget --quiet -O /var/local/status/google-chrome-stable_current_amd64.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt -y install /var/local/status/google-chrome-stable_current_amd64.deb
+# Install Chrome browser
+/var/local/installers/install-chrome-browser.sh
 
 # Populate About_This_Install directory with info specific to this run of the installer
 
