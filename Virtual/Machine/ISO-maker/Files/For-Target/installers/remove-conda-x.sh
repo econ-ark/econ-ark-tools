8f898341de3467f4ecd5286a8f53102a45501512
llorracc <git@llorracc.org>
2022-11-19 11:38:38 -0500

    Update remove-conda-x.sh

--- Virtual/Machine/ISO-maker/Files/For-Target/installers/remove-conda-x.sh
+++ Virtual/Machine/ISO-maker/Files/For-Target/installers/remove-conda-x.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+cd /home
+for dir in */; do  
+    user=$(basename $dir)
+    if id "$user" >/dev/null 2>&1; then # user exists
+	bashrc="/home/$user/.bashrc"
+	if [[ -e $bashrc ]]; then
+	    cmd="sudo -u $user /usr/local/$CHOSEN/bin/conda init --system --reverse bash"
+	    echo "$cmd"
+	    sudo /bin/bash -c "$cmd"
+	fi
+    fi
+done
+
 sudo conda install -c conda-forge anaconda-clean
+
 anaconda-clean --yes
-rm -Rf /usr/local/anaconda3
+
+rm -Rf /usr/local/anaconda
