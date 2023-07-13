674d36b22d98e9ec3fbef06b83c993fdc330a3d1
llorracc <git@llorracc.org>
2023-07-13 11:28:43 -0400

    Improve install-gh-tools

--- Virtual/Machine/ISO-maker/Files/For-Target/installers/install-gh-cli-tools.sh
+++ Virtual/Machine/ISO-maker/Files/For-Target/installers/install-gh-cli-tools.sh
@@ -2,10 +2,18 @@
 
 [[ -e /var/local/status/verbose ]] && set -x && set -v 
 
+# 20230711: 
+# From https://github.com/cli/cli/blob/trunk/docs/install_linux.md 
 # Install gh github command line tools 
-curl -SL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
-if [[ "$?" == 0 ]]; then  # command succeeded
-    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+
+# Get curl if not already present 
+type -p curl >/dev/null || (sudo apt update && sudo apt install curl -y)
+
+# Get keyring
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg 
+if [[ "$?" == 0 ]]; then  # curl command succeeded
+    sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
+    && echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
     sudo apt update
     sudo apt -y install gh
 else
