be7ed226ddf7b78640e7f69764a410b8c4073110
llorracc <git@llorracc.org>
2023-08-10 22:03:18 -0400

    tweak to install-gh-cli-tools

--- Virtual/Machine/ISO-maker/Files/For-Target/installers/install-gh-cli-tools.sh
+++ Virtual/Machine/ISO-maker/Files/For-Target/installers/install-gh-cli-tools.sh
@@ -12,8 +12,7 @@ type -p curl >/dev/null || (sudo apt update && sudo apt install curl -y)
 # Get keyring
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg 
 if [[ "$?" == 0 ]]; then  # curl command succeeded
-    sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
-    && echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+    sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg && echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
     sudo apt update
     sudo apt -y install gh
 else
