3799b7cf559da8de26b41b6d8fee2d103bae7e4a
llorracc <git@llorracc.org>
2023-10-03 09:20:35 -0400

    Fix hostname omission of short_hash

--- Virtual/Machine/ISO-maker/Files/For-Target/finish.sh
+++ Virtual/Machine/ISO-maker/Files/For-Target/finish.sh
@@ -27,7 +27,7 @@ default_domain=""
 build_date="$(</var/local/status/build_date.txt)"
 build_time="$(</var/local/status/build_time.txt)"
 
-new_hostname="xubark-$commit_date-$commit_hash"
+new_hostname="xubark-$commit_date-$short_hash"
 # short hostname: xubark+date of commit
 [[ ! -e /var/local/status/verbose ]] && new_hostname="xubark-$commit_date" && echo "$new_hostname" > /var/local/status/new_hostname
 
