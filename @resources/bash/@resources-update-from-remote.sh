2a7cf180e3682a63f3b876d66b2125a38e37afa8
llorracc <git@llorracc.org>
2024-07-28 12:36:23 -0400

    "cmd_dryrun"

--- @resources/bash/@resources-update-from-remote.sh
+++ @resources/bash/@resources-update-from-remote.sh
@@ -108,7 +108,7 @@ opts=(
 cmd_dryrun='rsync --dry-run '"${opts[@]}"' '"$orig_path/@resources/"' '"$dest_path/@resources/"
 echo $cmd_dryrun
 #deletions=$(rsync --dry-run "${opts[@]}" "$orig_path/@resources/" "$dest_path/@resources/" | grep deleting)
-deletions=$($(eval $cmd_dryrun) | grep deleting)
+deletions=$($(eval "$cmd_dryrun") | grep deleting)
 
        cmd='rsync '"$dryrun"' '"${opts[@]}"' '"$orig_path/@resources/"' '"$dest_path/@resources/"
 
