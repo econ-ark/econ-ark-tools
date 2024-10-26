9d33c41a376a2ffeef7728c84fbb3b014101a11a
Christopher Llorracc Carroll <1320319+llorracc@users.noreply.github.com>
2024-10-26 16:48:35 -0400

    interleave with texmf-local-git

--- @resources/texlive/texmf-local/scripts/make4ht/gh-pages-delete.sh
+++ @resources/texlive/texmf-local/scripts/make4ht/gh-pages-delete.sh
@@ -1,1 +1,16 @@
-/Volumes/Sync/GitHub/llorracc/SolvingMicroDSOPs/SolvingMicroDSOPs-make/gh-pages-delete.sh
+#!/bin/bash
+
+# Delete remote gh-pages if it exists
+if git ls-remote --exit-code --heads origin gh-pages > /dev/null 2>&1; then
+    git push origin --delete gh-pages
+fi
+
+# Delete local gh-pages if it exists
+if git show-ref --verify --quiet refs/heads/gh-pages; then
+    git branch -D gh-pages
+    # It lingers and can be restored by git checkout unless you clear cache:
+    git reflog expire --expire=now --all
+    git gc --prune=now 
+    sleep 2 # give it a couple of secs to process
+fi
+
